77646633785db6d0889deb66cce797fd25467495
Klippie <info@johanvanderklift.nl>
2024-01-01 21:53:09 +0100

    Added PostController

---
+++ Dtos/PostToEditDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetAPI.Dtos;
+
+public class PostToEditDto
+{
+    public int PostId { get; set; }
+    public string PostTitle { get; set; } = "";
+    public string PostContent { get; set; } = "";
+}
