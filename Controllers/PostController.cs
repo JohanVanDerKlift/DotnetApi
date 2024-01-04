7d9979bf33777ae5bcbc4ac7e1c9645ef9985867
Klippie <info@johanvanderklift.nl>
2024-01-04 22:04:49 +0100

    Added stored procedures

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetAPI.Controllers;
 
-[Authorize]
+// [Authorize]
 [ApiController]
 [Route("[controller]")]
 public class PostController : ControllerBase
@@ -18,57 +18,39 @@ public class PostController : ControllerBase
         _dapper = new DataContextDapper(config);
     }
 
-    [HttpGet("Posts")]
-    public IEnumerable<Post> GetPosts()
+    [HttpGet("Posts/{postId}/{userId}/{searchParam}")]
+    public IEnumerable<Post> GetPosts(int postId = 0, int userId = 0, string searchParam = "None")
     {
-        string sql = @"SELECT [PostId],
-            [UserId],
-            [PostTitle],
-            [PostContent],
-            [PostCreated],
-            [PostUpdated] FROM TutorialAppSchema.Posts";
+        string sql = @"EXEC TutorialAppSchema.spPosts_Get";
+        string parameters = "";
+
+        if (postId != 0)
+        {
+            parameters += $", @PostId = {postId}";
+        }
+
+        if (userId != 0)
+        {
+            parameters += $", @UserId = {userId}";
+        }
+
+        if (searchParam.ToLower() != "none")
+        {
+            parameters += $", @SearchValue = '{searchParam}'";
+        }
+
+        if (parameters.Length > 0)
+        {
+            sql += parameters.Substring(1);
+        }
 
         return _dapper.LoadData<Post>(sql);
     }
     
-    [HttpGet("PostSingle/{postId}")]
-    public Post GetPostSingle(int postId)
-    {
-        string sql = $@"SELECT [PostId],
-            [UserId],
-            [PostTitle],
-            [PostContent],
-            [PostCreated],
-            [PostUpdated] FROM TutorialAppSchema.Posts
-            WHERE PostId = {postId.ToString()}";
-
-        return _dapper.LoadDataSingle<Post>(sql);
-    } 
-    
-    [HttpGet("PostsByUser/{userId}")]
-    public IEnumerable<Post> GetPostsByUser(int userId)
-    {
-        string sql = $@"SELECT [PostId],
-            [UserId],
-            [PostTitle],
-            [PostContent],
-            [PostCreated],
-            [PostUpdated] FROM TutorialAppSchema.Posts
-            WHERE UserId = {userId.ToString()}";
-
-        return _dapper.LoadData<Post>(sql);
-    } 
-    
     [HttpGet("MyPosts")]
     public IEnumerable<Post> GetMyPosts()
     {
-        string sql = $@"SELECT [PostId],
-            [UserId],
-            [PostTitle],
-            [PostContent],
-            [PostCreated],
-            [PostUpdated] FROM TutorialAppSchema.Posts
-            WHERE UserId = {this.User.FindFirst("userId")?.Value}";
+        string sql = $"EXEC TutorialAppSchema.spPosts_Get @UserId = '{this.User.FindFirst("userId").Value}'";
 
         return _dapper.LoadData<Post>(sql);
     }
@@ -109,7 +91,8 @@ public class PostController : ControllerBase
     [HttpDelete("Post/{postId}")]
     public IActionResult DeletePost(int postId)
     {
-        string sql = $"DELETE FROM TutorialAppSchema.Posts WHERE PostId = {postId.ToString()}";
+        string sql = $"DELETE FROM TutorialAppSchema.Posts WHERE PostId = {postId.ToString()} " +
+                     $"AND UserId = {this.User.FindFirst("userId")?.Value}";
         if (_dapper.ExecuteSql(sql))
         {
             return Ok();
